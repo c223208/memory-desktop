647444e34a83f62569dd203536f7abc14fbded9b
c223208 <c223208@ed.nagoya-cu.ac.jp>
2026-01-29 20:10:40 +0900

    メッセージ内容反映

--- script/config.js
+++ script/config.js
@@ -6,8 +6,8 @@ const windowsConfig = [
     { id: 'photo3', type: 'photo-window', size: 'window-medium', title: 'Picture 3', top: 210, left: 100 },
     { id: 'photo4', type: 'photo-window', size: 'window-large', title: 'Picture 4', top: 310, left: 400 },
     { id: 'photo5', type: 'photo-window', size: 'window-medium', title: 'Picture 5', top: 60, left: 750 },
-    { id: 'msg1', type: 'message-window', title: 'まおからのメッセージ', top: 360, left: 150, content: 'message/miwa.html' },
-    { id: 'msg2', type: 'message-window', title: '重要なお知らせ～みづこ', top: 410, left: 600, content: 'message/miduko.html' },
+    { id: 'msg1', type: 'message-window', title: 'ご指導への御礼（三和）', top: 360, left: 150, content: 'message/miwa.html' },
+    { id: 'msg2', type: 'message-window', title: '（木下）', top: 410, left: 600, content: 'message/miduko.html' },
     { id: 'msg4', type: 'message-window', title: '本をもとめて', top: 510, left: 700, content: 'window/mice.html' },
     { id: 'msg5', type: 'message-window', title: 'イタリア周辺（2c BC~15c AD）', top: 100, left: 300, content: 'window/italy.html' }
 ];
